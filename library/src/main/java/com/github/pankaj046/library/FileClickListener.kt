1d5cc31cedd57af3443ed14db7fc484932a9049e
pankaj <pankaj_046@yahoo.in>
2024-03-17 16:27:55 +0530

    minor changes

--- library/src/main/java/com/github/pankaj046/library/FileClickListener.kt
+++ library/src/main/java/com/github/pankaj046/library/FileClickListener.kt
@@ -3,6 +3,6 @@ package com.github.pankaj046.library
 import java.io.File
 
 interface FileClickListener {
-    fun singleSelection(path: String?)
-    fun multiSelection(paths: HashSet<String>?)
+    fun singleSelection(path: String?) {}
+    fun multiSelection(paths: HashSet<String>?) {}
 }
